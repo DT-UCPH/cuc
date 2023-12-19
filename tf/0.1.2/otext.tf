f59b480b692267bbc0147707a9505f7943714203
ch-jensen <hojer88@gmail.com>
2023-12-19 10:46:52 +0100

    new TF

---
+++ tf/0.1.2/otext.tf
@@ -0,0 +1,16 @@
+@config
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=cuc
+@datasetName=Copenhagen Ugarit Corpus
+@encodedBy=Christian Canu Højgaard and Martijn Naaijer
+@fmt:text-orig-full={sign}
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=
+@sectionFeatures=corpus,column,line
+@sectionTypes=corpus,column,line
+@source=
+@version=0.1.2
+@writtenBy=Text-Fabric
+@dateWritten=2023-12-15T14:23:53Z
+
---
+++ tf/0.1.2/otype.tf
@@ -0,0 +1,19 @@
+@node
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=cuc
+@datasetName=Copenhagen Ugarit Corpus
+@encodedBy=Christian Canu Højgaard and Martijn Naaijer
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=
+@source=
+@valueType=str
+@version=0.1.2
+@writtenBy=Text-Fabric
+@dateWritten=2023-12-15T14:23:52Z
+
+1-3850	sign
+3851-3854	column
+3855	corpus
+3856-3984	line
+3985-4940	word
