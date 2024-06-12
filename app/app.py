48847046b643d81d0cc10838db4edf447298f007
ch-jensen <hojer88@gmail.com>
2024-06-12 21:45:55 +0200

    update app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,6 @@
 import types
 from tf.advanced.app import App
 
-
-CERTAINTY = dict(
-    uncertain="uncertain",
-)
-
 EMENDATION = dict(
     restored="supplied",
     excised="excised",
@@ -28,21 +23,20 @@ class TfApp(App):
         app.fmt_layout = types.MethodType(fmt_layoutTrans, app)
         super().__init__(*args, **kwargs)
 
+
     def _wrapHtml(app, n, kind):
         api = app.api
         F = api.F
-        L = api.L
 
-        material = (F.usign.v(n) if kind == "u" else F.sign.v(n)) or ""
+        after = (F.traileru.v(n) if kind == "u" else F.trailer.v(n)) or ""
+        material = (F.signu.v(n) if kind == "u" else F.sign.v(n)) or ""
         emendation = F.emen.v(n)
-        certainty = F.cert.v(n)
-        material = f"""<span class="{CERTAINTY.get(certainty, None)} {EMENDATION.get(emendation, None)}">{material}</span>"""
+        cls = EMENDATION.get(emendation, None)
 
-        after = (F.utrailer.v(L.u(n, 'word')[0]) if kind == "u" else F.trailer.v(L.u(n, 'word')[0])) or ""
-       	trailer_emendation = F.trailer_emen.v(L.u(n, 'word')[0])
-        after = f"""<span class="{CERTAINTY.get(trailer_emendation, None)}">{after}</span>"""
+        if cls is not None:
+             material = f"""<span class="{cls}">{material}</span>"""
 
-        if n == L.d(L.u(n, 'word')[0], 'sign')[-1]:
-            return f"{material}{after}"
-        else:
-            return material
+        if F.cert.v(n) != 'uncertain':
+            material = f"""<em>{material}</em>"""
+
+        return f"{material}{after}"
--- tests/utils/files_test.py
+++ tests/utils/files_test.py
@@ -9,6 +9,19 @@ import docx
 CHARACTERS = {'a','b','d','ḏ','g','ġ','h','ḥ','ḫ','i','k','l','m','n','p','q','r','s','ṣ','š','ṯ','ṭ','t','u','w','y',
               'z','ẓ','ʿ','x','.',' ','\xa0','-','…','[',']','{','}','<','>','(',')','/','\\','\n'}
 
+LATIN_NUMBERS = {
+'I':1,
+'II':2,
+'III':3,
+'IV':4,
+'V':5,
+'VI':6,
+'VII':7,
+'VIII':8,
+'IX':9,
+'X':10,
+}
+
 class TextTranscriber:
     """
     Makes a transcription of original word files line by line
@@ -16,11 +29,12 @@ class TextTranscriber:
 
     def __init__(self, WORD_FILES_FOLDER):
         self.word_files = [f for f in os.listdir(WORD_FILES_FOLDER) if f.lower().endswith('.docx') or f.lower().endswith('.doc')]
+        print(sorted(self.word_files))
         
         for file in self.word_files:
             path = os.path.join(WORD_FILES_FOLDER, file)
             self.word_doc = docx.Document(path)
-            run_test = self.test()
+            run_test = self.test(file)
             if run_test:
                 print(run_test)
                 break
@@ -28,17 +42,22 @@ class TextTranscriber:
                 print(f"{self.corpus}: SUCCESS")
                 
         
-    def test(self):
+    def test(self, filename):
         '''
         Testing each sign of each line against accepted characters.
         '''
+        col_nu = 0
         for para in self.word_doc.paragraphs:
             if re.match('KTU', para.text):
                 self.corpus = para.text
-                continue
+                if self.corpus != filename[:filename.index('_')]:
+                    return f'{filename}: Corpus name does not match file name'
 
             if re.match('[LXVI+]', para.text):
                 column = para.text
+                col_nu+=1
+                if col_nu != LATIN_NUMBERS[column]: #Check if all columns are included in the right order
+                    return f'Unexpected column number in {self.corpus}: {para.text}. Expected column number: {col_nu}'
 
             side = 'le\.e\.|low\.e\.|obv\.|r\.e\.|rev\.|up\.e\.'
             if re.match(rf'({side}) ?.', para.text):
@@ -54,6 +73,18 @@ class TextTranscriber:
                 for sign in text:
                     if sign not in CHARACTERS:
                         return f'Illegal character in {self.corpus} {column} {line}: {sign} {ord(sign)}'
+
+                if text.count('[') != text.count(']'):
+                    return f'[ does not match ] in {self.corpus} {column} {line}'
+
+                if text.count('{') != text.count('}'):
+                    return r'\{ does not match \} in {self.corpus} {column} {line}'
+
+                if text.count('<') != text.count('>'):
+                    return f'< does not match > in {self.corpus} {column} {line}'
+
+                if text.count('(') != text.count(')'):
+                    return f'( does not match ) in {self.corpus} {column} {line}'
                 
 WORD_FILES_FOLDER = './files'
         
