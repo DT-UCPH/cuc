3c833d31708677986a3ccc3bd2a7ac808b0bfca4
ch-jensen <hojer88@gmail.com>
2024-07-29 14:10:48 +0200

    TF 0.2.3

---
+++ tf/0.2.3/otype.tf
@@ -0,0 +1,19 @@
+@node
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=cuc
+@datasetName=Copenhagen Ugarit Corpus
+@encodedBy=Christian Canu Højgaard and Martijn Naaijer
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=
+@source=
+@valueType=str
+@version=0.2.3
+@writtenBy=Text-Fabric
+@dateWritten=2024-07-29T12:03:41Z
+
+1-58901	sign
+58902-58999	column
+59000-62119	line
+62120-62165	tablet
+62166-74491	word
---
+++ tf/0.2.3/tablet.tf
@@ -0,0 +1,61 @@
+@node
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=cuc
+@datasetName=Copenhagen Ugarit Corpus
+@description=tablet name
+@encodedBy=Christian Canu Højgaard and Martijn Naaijer
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=
+@source=
+@valueType=str
+@version=0.2.3
+@writtenBy=Text-Fabric
+@dateWritten=2024-07-29T12:03:41Z
+
+62120	KTU 1.1
+KTU 1.2
+KTU 1.3
+KTU 1.4
+KTU 1.5
+KTU 1.6
+KTU 1.7
+KTU 1.14
+KTU 1.15
+KTU 1.16
+KTU 1.17
+KTU 1.18
+KTU 1.19
+KTU 1.20
+KTU 1.21
+KTU 1.22
+KTU 2.5
+KTU 2.6
+KTU 2.7
+KTU 2.8
+KTU 2.9
+KTU 2.10
+KTU 2.11
+KTU 2.12
+KTU 2.13
+KTU 2.14
+KTU 2.15
+KTU 2.16
+KTU 2.17
+KTU 2.18
+KTU 2.20
+KTU 2.21
+KTU 2.22
+KTU 2.23
+KTU 2.24
+KTU 2.25
+KTU 2.26
+KTU 2.27
+KTU 2.30
+KTU 2.31
+KTU 2.32
+KTU 2.34
+KTU 2.35
+KTU 2.36
+KTU 2.37
+KTU 2.38
