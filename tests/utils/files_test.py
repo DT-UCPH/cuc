346ab3b622e361bd8e8472d6a81e9fbd4ef953a7
ch-jensen <hojer88@gmail.com>
2024-08-20 18:34:36 +0200

    improved tests

--- tests/utils/files_test.py
+++ tests/utils/files_test.py
@@ -7,7 +7,9 @@ import re
 import docx
 
 CHARACTERS = {'a','b','d','ḏ','g','ġ','h','ḥ','ḫ','i','k','l','m','n','p','q','r','s','ṣ','š','ṯ','ṭ','t','u','w','y',
-              'z','ẓ','ʿ','x','.',' ','\xa0','-','…','[',']','{','}','<','>','(',')','/','\\','\n'}
+              'z','ẓ','ʿ','x','.',' ','\xa0','-','[',']','{','}','<','>','(',')','/','\\','\n','…'}
+
+side = 'le\.e\.|low\.e\.|obv\.|r\.e\.|rev\.|up\.e\.'
 
 LATIN_NUMBERS = {
 'I':1,
@@ -29,7 +31,7 @@ class TextTranscriber:
 
     def __init__(self, WORD_FILES_FOLDER):
         self.word_files = [f for f in os.listdir(WORD_FILES_FOLDER) if f.lower().endswith('.docx') or f.lower().endswith('.doc')]
-        print(sorted(self.word_files))
+        #print(sorted(self.word_files))
         
         for file in self.word_files:
             path = os.path.join(WORD_FILES_FOLDER, file)
@@ -40,35 +42,51 @@ class TextTranscriber:
                 break
             else:
                 print(f"{self.corpus}: SUCCESS")
-                
         
     def test(self, filename):
         '''
         Testing each sign of each line against accepted characters.
         '''
         col_nu = 0
+        line_nu = 0
         for para in self.word_doc.paragraphs:
+
             if re.match('KTU', para.text):
                 self.corpus = para.text
                 if self.corpus != filename[:filename.index('_')]:
                     return f'{filename}: Corpus name does not match file name'
 
-            if re.match('[LXVI+]', para.text):
+            elif re.fullmatch('[LXVI]+', para.text):
                 column = para.text
                 col_nu+=1
+                line_nu = 1
                 if col_nu != LATIN_NUMBERS[column]: #Check if all columns are included in the right order
                     return f'Unexpected column number in {self.corpus}: {para.text}. Expected column number: {col_nu}'
 
-            side = 'le\.e\.|low\.e\.|obv\.|r\.e\.|rev\.|up\.e\.'
-            if re.match(rf'({side}) ?.', para.text):
-                return f'Side annotation mixed with text in {self.corpus} {column} {line}: {para.text}'
+            elif re.match(rf'({side})', para.text):
+                continue
 
-            if para.text == '...':
+            elif para.text == '...' or para.text == '. . .':
                 return f'Unexpected ... in {self.corpus} {column}: {para.text}'
 
-            if re.match('^\d+', para.text):
+            elif re.match('^…', para.text):
+                continue
+
+            elif re.match('^-', para.text):
+                continue
+
+            elif not para.text:
+                continue
+
+            elif re.match('^\d+', para.text):
                 line = int(re.match('^\d+', para.text).group())
                 text = re.sub('^\d+', '', para.text).lstrip()
+
+                if not col_nu:
+                    return f'Missing column number in {self.corpus}'
+
+                if line != line_nu:
+                    return f'Unexpected line number in {self.corpus} {column} {line}'
                 
                 for sign in text:
                     if sign not in CHARACTERS:
@@ -85,7 +103,16 @@ class TextTranscriber:
 
                 if text.count('(') != text.count(')'):
                     return f'( does not match ) in {self.corpus} {column} {line}'
-                
+                line_nu += 1
+
+            else:
+                ERROR_RESPONSE = f'Unexpected (initial?) sign in {self.corpus}'
+                if col_nu != 0:
+                     ERROR_RESPONSE += f' {column}'
+                if line:
+                     ERROR_RESPONSE += f' {str(line)}'
+                return f'{ERROR_RESPONSE}: {para.text}'
+     
 WORD_FILES_FOLDER = './files'
         
 if __name__ == '__main__':
