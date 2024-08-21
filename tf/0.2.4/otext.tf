aec4545e13f427e18a45ccaeca56e36f56b83892
ch-jensen <hojer88@gmail.com>
2024-08-21 09:55:31 +0200

    many more tablets

---
+++ tf/0.2.4/otext.tf
@@ -0,0 +1,17 @@
+@config
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=cuc
+@datasetName=Copenhagen Ugarit Corpus
+@encodedBy=Christian Canu Højgaard and Martijn Naaijer
+@fmt:text-orig-full={sign}
+@fmt:text-orig-unicode={usign}
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=
+@sectionFeatures=tablet,column,line
+@sectionTypes=tablet,column,line
+@source=
+@version=0.2.4
+@writtenBy=Text-Fabric
+@dateWritten=2024-08-21T07:49:11Z
+
---
+++ tf/0.2.4/otype.tf
@@ -0,0 +1,19 @@
+@node
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=cuc
+@datasetName=Copenhagen Ugarit Corpus
+@encodedBy=Christian Canu Højgaard and Martijn Naaijer
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=
+@source=
+@valueType=str
+@version=0.2.4
+@writtenBy=Text-Fabric
+@dateWritten=2024-08-21T07:49:11Z
+
+1-75120	sign
+75121-75280	column
+75281-79522	line
+79523-79627	tablet
+79628-95128	word
