ee7611654ea0d83ce6a5961fd13b93c475a71ff3
ch-jensen <hojer88@gmail.com>
2026-01-08 12:04:27 +0100

    new TF

---
+++ tf/0.2.6/otype.tf
@@ -0,0 +1,19 @@
+@node
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=cuc
+@datasetName=Copenhagen Ugarit Corpus
+@encodedBy=Christian Canu Højgaard and Martijn Naaijer
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=
+@source=
+@valueType=str
+@version=0.2.6
+@writtenBy=Text-Fabric
+@dateWritten=2026-01-08T10:53:41+00:00Z
+
+1-127355	sign
+127356-127688	column
+127689-135265	line
+135266-135543	tablet
+135544-162226	word
